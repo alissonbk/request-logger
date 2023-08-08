6880899b08d4520a6a0d38a7291d2d4dfeea7a5f
alissonbk <alissonbk2010@hotmail.com>
2023-08-08 11:06:19 -0300

    other fields

--- src/request/content.go
+++ src/request/content.go
@@ -8,22 +8,25 @@ import (
 )
 
 type Content struct {
-	Method         string
-	HttpVersion    string
-	Host           string
-	Connection     string
-	Server         string
-	ContentLength  int32
-	CacheControl   string
-	Origin         string
-	ContentType    []string
-	UserAgent      string
-	Accept         string
-	Referer        string
-	AcceptEncoding string
-	AcceptLanguage string
-	Date           string // FIXME: use date
-	Payload        string
+	Method           string
+	HttpVersion      string
+	Host             string
+	Connection       string
+	Server           string
+	CacheControl     string
+	Origin           string
+	UserAgent        string
+	Accept           []string
+	AcceptEncoding   string
+	AcceptLanguage   []string
+	Referer          string
+	TransferEncoding string
+	XPoweredBy       string
+	Date             string // FIXME: use date
+	ContentType      []string
+	ContentEncoding  string
+	ContentLength    int32
+	Payload          string
 }
 
 // BuildRequestContent Receive packet as a string and return the content
@@ -42,16 +45,24 @@ func HasReadableContent(str string) bool {
 }
 
 func (content *Content) IterateAndSetData(packet string) {
+	content.FindUserAgent(packet)
+	content.FindDate(packet)
+	content.FindServer(packet)
+	content.FindAcceptEncoding(packet)
 	words := strings.Fields(packet)
 	for index, field := range words {
 		content.FindMethod(field)
 		content.FindHttpVersion(field)
-		content.FindServer(field, index, words)
 		content.FindHost(field, index, words)
 		content.FindConnection(field, index, words)
 		content.FindReferer(field, index, words)
 		content.FindCacheControl(field, index, words)
-		content.FindDate(packet)
+		content.FindXPoweredBy(field, index, words)
+		content.FindContentEncoding(field, index, words)
+		content.FindTransferEnconding(field, index, words)
+		content.FindAccept(field, index, words)
+		content.FindAcceptLanguage(field, index, words)
+		content.FindContentLength(field, index, words)
 	}
 }
 
@@ -63,11 +74,12 @@ func (content *Content) FindHttpVersion(field string) {
 	}
 }
 
-func (content *Content) FindServer(field string, index int, words []string) {
-	f := strings.ToUpper(field)
-	if strings.Contains(f, "SERVER:") {
-		nextWord := words[index+1]
-		content.Server = nextWord
+func (content *Content) FindServer(packet string) {
+	raw := strconv.Quote(packet)
+	_, after, _ := strings.Cut(raw, "Server:")
+	before, _, _ := strings.Cut(after, "\\r\\n")
+	if before != "" {
+		content.Server = strings.TrimSpace(before)
 	}
 }
 
@@ -105,12 +117,81 @@ func (content *Content) FindReferer(field string, index int, words []string) {
 
 func (content *Content) FindDate(packet string) {
 	raw := strconv.Quote(packet)
-	_, after, _ := strings.Cut(strings.ToUpper(raw), "DATE:")
-	before, _, _ := strings.Cut(after, "\\R\\N")
+	_, after, _ := strings.Cut(raw, "Date:")
+	before, _, _ := strings.Cut(after, "\\r\\n")
 	if before != "" {
 		content.Date = strings.TrimSpace(before)
 	}
+}
 
+func (content *Content) FindXPoweredBy(field string, index int, words []string) {
+	f := strings.ToUpper(field)
+	if strings.Contains(f, "X-POWERED-BY:") {
+		nextWord := words[index+1]
+		content.XPoweredBy = nextWord
+	}
+}
+
+func (content *Content) FindContentEncoding(field string, index int, words []string) {
+	f := strings.ToUpper(field)
+	if strings.Contains(f, "CONTENT-ENCODING:") {
+		nextWord := words[index+1]
+		content.ContentEncoding = nextWord
+	}
+}
+
+func (content *Content) FindTransferEnconding(field string, index int, words []string) {
+	f := strings.ToUpper(field)
+	if strings.Contains(f, "TRANSFER-ENCODING:") {
+		nextWord := words[index+1]
+		content.TransferEncoding = nextWord
+	}
+}
+
+func (content *Content) FindAccept(field string, index int, words []string) {
+	f := field
+	if strings.Contains(f, "Accept:") {
+		acceptWords := strings.Split(words[index+1], ";")
+		content.Accept = acceptWords
+	}
+}
+
+func (content *Content) FindAcceptLanguage(field string, index int, words []string) {
+	f := field
+	if strings.Contains(f, "Accept-Language:") {
+		acceptWords := strings.Split(words[index+1], ";")
+		content.AcceptLanguage = acceptWords
+	}
+}
+
+func (content *Content) FindContentLength(field string, index int, words []string) {
+	f := strings.ToUpper(field)
+	if strings.Contains(f, "Content-Length:") {
+		nextWord := words[index+1]
+		cLength, err := strconv.ParseInt(nextWord, 10, 32)
+		if err != nil {
+			log.Panic("Failed to convert Content Length to int32:", err)
+		}
+		content.ContentLength = int32(cLength)
+	}
+}
+
+func (content *Content) FindUserAgent(packet string) {
+	raw := strconv.Quote(packet)
+	_, after, _ := strings.Cut(raw, "User-Agent:")
+	before, _, _ := strings.Cut(after, "\\r\\n")
+	if before != "" {
+		content.UserAgent = strings.TrimSpace(before)
+	}
+}
+
+func (content *Content) FindAcceptEncoding(packet string) {
+	raw := strconv.Quote(packet)
+	_, after, _ := strings.Cut(raw, "Accept-Encoding:")
+	before, _, _ := strings.Cut(after, "\\r\\n")
+	if before != "" {
+		content.AcceptEncoding = strings.TrimSpace(before)
+	}
 }
 
 // FindMethod Find the used method from the given string
